67750b6cd627c91df40c4c495e2a3f902a87cb5e
bakuvi <emin.feyziyev@gmail.com>
2020-11-14 18:31:18 +0300

    first commit'

---
+++ src/sample/DBconnection.java
@@ -0,0 +1,57 @@
+package sample;
+
+
+import java.sql.*;
+
+public class DBconnection {
+    private final String HOST = "localhost";
+    private final String PORT = "3306";
+    private final String DB_NAME = "authorizationfxproject";
+    private final String LOGIN = "root";
+    private final String PASS = "514146454";
+
+    private Connection dbConn = null;
+
+    private Connection getConn() throws ClassNotFoundException, SQLException {
+        String connStr = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DB_NAME +
+                "?characterEncoding=UTF-8&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        dbConn = DriverManager.getConnection(connStr, LOGIN, PASS);
+        return dbConn;
+    }
+
+    public void isConnected() throws SQLException, ClassNotFoundException {
+        dbConn = getConn();
+        System.out.println(dbConn.isValid(1000));
+    }
+
+    public boolean regUser(String login, String email, String password) {
+        String sql = "INSERT INTO `users` (`login`, `email`, `password`) VALUES (?, ?, ?)";
+        try {
+            Statement statement = getConn().createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM `users` WHERE LOGIN = '" + login + "' LIMIT 1");
+            if (resultSet.next())
+                return false;
+
+            PreparedStatement preparedStatement = getConn().prepareStatement(sql);
+            preparedStatement.setString(1, login);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+        return true;
+
+    }
+
+    public boolean authUser(String login, String password) throws SQLException, ClassNotFoundException {
+         Statement statement = getConn().createStatement();
+        String sql = "SELECT * FROM `users` WHERE `login` = '" + login + "' AND `password` = '" + password + "' LIMIT 1";
+        ResultSet resultSet = statement.executeQuery(sql);
+        return resultSet.next();
+    }
+}
---
+++ src/sample/controllers/RegController.java
@@ -0,0 +1,133 @@
+package sample.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import sample.DBconnection;
+
+import java.awt.event.ActionEvent;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+
+public class RegController {
+    DBconnection db = new DBconnection();
+
+    @FXML
+    private TextField login_reg;
+
+    @FXML
+    private TextField email_reg;
+
+    @FXML
+    private PasswordField pass_reg;
+
+    @FXML
+    private CheckBox confidentials;
+
+    @FXML
+    private Button btn_reg;
+
+    @FXML
+    private TextField log_auth;
+
+    @FXML
+    private PasswordField pass_auth;
+
+    @FXML
+    private Button btn_auth;
+
+    @FXML
+    private void initialize() {
+        btn_reg.setOnAction(Event -> {
+            login_reg.setStyle("-fx-border-color: #fafafa");
+            email_reg.setStyle("-fx-border-color: #fafafa");
+            pass_reg.setStyle("-fx-border-color: #fafafa");
+            btn_reg.setText("Зарегистрироваться");
+
+            if (login_reg.getCharacters().length() < 3) {
+                login_reg.setStyle("-fx-border-color: red");
+                return;
+            } else if (email_reg.getCharacters().length() < 3) {
+                email_reg.setStyle("-fx-border-color: red");
+                return;
+            } else if (pass_reg.getCharacters().length() < 3) {
+                pass_reg.setStyle("-fx-border-color: red");
+                return;
+            } else if (!confidentials.isSelected()) {
+                btn_reg.setText("Поставьте галочку");
+                return;
+            }
+
+
+            String pass = md5String(pass_reg.getCharacters().toString());
+            boolean isReg = db.regUser(login_reg.getCharacters().toString(), email_reg.getCharacters().toString(), pass);
+            if (isReg) {
+                login_reg.setText("");
+                email_reg.setText("");
+                pass_reg.setText("");
+                btn_reg.setText("Готово");
+            } else {
+                btn_reg.setText("Введите другой логин");
+            }
+        });
+        btn_auth.setOnAction(e -> {
+            log_auth.setStyle("-fx-border-color: #fafafa");
+            pass_auth.setStyle("-fx-border-color: #fafafa");
+
+            if (log_auth.getCharacters().length() < 3) {
+                log_auth.setStyle("-fx-border-color: red");
+                return;
+
+            } else if (pass_auth.getCharacters().length() < 3) {
+                pass_auth.setStyle("-fx-border-color: red");
+                return;
+            }
+
+
+            String pass = md5String(pass_auth.getCharacters().toString());
+
+
+            try {
+              boolean  isAuth = db.authUser(log_auth.getCharacters().toString(), pass);
+                if (isAuth) {
+                    log_auth.setText("");
+                    pass_auth.setText("");
+                    btn_auth.setText("Готово");
+                } else {
+                    btn_auth.setText("Не найден");
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            } catch (ClassNotFoundException classNotFoundException) {
+                classNotFoundException.printStackTrace();
+            }
+
+
+
+        });
+    }
+
+    public static String md5String(String pass) {
+        MessageDigest messageDigest = null;
+        byte[] digest = new byte[0];
+
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.reset();
+            messageDigest.update(pass.getBytes());
+            digest = messageDigest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        BigInteger bigInteger = new BigInteger(1, digest);
+        String m5dHex = bigInteger.toString(16);
+
+        while (m5dHex.length() < 32)
+            m5dHex = "0" + m5dHex;
+
+        return m5dHex;
+    }
+
+}
